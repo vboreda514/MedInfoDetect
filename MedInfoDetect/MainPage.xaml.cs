852575b1de159532fe20b1af095d3fad42d59c5e
Viraj Boreda <virajboreda514@gmail.com>
2019-08-15 10:23:34 -0400

    separates name, sex, and mr#

--- MedInfoDetect/MainPage.xaml.cs
+++ MedInfoDetect/MainPage.xaml.cs
@@ -10,7 +10,9 @@ using Xamarin.Forms;
 using Tesseract;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+
 
 namespace MedInfoDetect
 {
@@ -21,14 +23,14 @@ namespace MedInfoDetect
     public partial class MainPage : ContentPage
     {
         private readonly ITesseractApi api;
-
+        
         public MainPage()
         {
             
             InitializeComponent();
             api = DependencyService.Get<ITesseractApi>();
 
-            CameraButton.Clicked += async (sender, args) =>
+            /*CameraButton.Clicked += async (sender, args) =>
             {
                 
                 await CrossMedia.Current.Initialize();
@@ -42,6 +44,7 @@ namespace MedInfoDetect
                 if (file == null)
                     return;
                 System.IO.Stream photoStream = file.GetStream();
+
                 
                 //photo.Source = ImageSource.FromStream(() =>
                 // {
@@ -51,10 +54,10 @@ namespace MedInfoDetect
                 // });
 
                 NameEntry.Text  = "1";
-                Photo.Source = ImageSource.FromStream(file.GetStream); //debug code
-                System.Drawing.Bitmap bitmap = new Bitmap(photoStream);
+                //Photo.Source = ImageSource.FromStream(file.GetStream); //debug code
+                Bitmap bitmap = new Bitmap(photoStream);
                 Stream stream2 = RaiseContrast(bitmap);
-                Photo.Source = ImageSource.FromStream(() => stream2);
+                //Photo.Source = ImageSource.FromStream(() => stream2);
                 bool initialised = await api.Init("eng");
                 NameEntry.Text += "2";
                 api.SetWhitelist("0123456789:-,#'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
@@ -91,13 +94,14 @@ namespace MedInfoDetect
                     NameEntry.Text = "Image Recognition Failed";
                 }
                 
-            };
+            }; */
             
         }
 
-        public MemoryStream RaiseContrast(System.Drawing.Bitmap bitmap1)
+        public Stream RaiseContrast(Bitmap bitmap1)
         {
             LockBitmap bitmap = new LockBitmap(bitmap1);
+
             bitmap.LockBits();
             var raisedby = 30.0;      //change this
             var contrast = Math.Pow((100.0 + raisedby) / 100.0, 2);
@@ -127,20 +131,106 @@ namespace MedInfoDetect
                 }
             }
             bitmap.UnlockBits();
-            MemoryStream m = new MemoryStream();
+            Stream m = new MemoryStream();
             m.Write(bitmap.Pixels, 0, bitmap.Pixels.Length);
             return m;
         }
 
 
-        private void CameraButton_Clicked(object sender, EventArgs e)
+        private async void CameraButton_Clicked(object sender, EventArgs e)
         {
-         
+
+            await CrossMedia.Current.Initialize();
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                Directory = "MedSticker",
+                Name = "sticker.jpg"
+            });
+
+            if (file == null)
+                return;
+            System.IO.Stream photoStream = file.GetStream();
+
+
+            //photo.Source = ImageSource.FromStream(() =>
+            // {
+            //     var stream = file.GetStream();
+            //file.Dispose();
+            //     return stream;
+            // });
+
+            //NameEntry.Text = "1";
+            //Photo.Source = ImageSource.FromStream(file.GetStream); //debug code
+            //Bitmap bitmap = new Bitmap(photoStream);
+            //Stream photoStream2 = RaiseContrast(bitmap);
+            //Photo.Source = ImageSource.FromStream(() => stream2);
+            bool initialised = await api.Init("eng");
+            //NameEntry.Text += "2";
+            api.SetWhitelist("0123456789:-,#'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
+            bool success = await api.SetImage(photoStream);
+            //NameEntry.Text += "3";
+            if (success)
+            {
+                //List<Result> lines = api.Results(PageIteratorLevel.Textline);
+                //List<Result> words = api.Results(PageIteratorLevel.Word);
+                List<Result> results = api.Results(PageIteratorLevel.Symbol).ToList();
+                //List<Result> blocks = api.Results(PageIteratorLevel.Block);
+
+                //List<Result> results = api.Res    ults(PageIteratorLevel.Paragraph).ToList();
+                var res = " ";
+                var conf = " ";
+                foreach (Result r in results)
+                {
+                    if (r.Confidence > 85f)
+                    {
+                        res += r.Text.ToUpper();
+                        conf += r.Confidence.ToString() + " ";
+                    }
+
+                }
+                res = res.Replace(":", "");
+                SexEntry.Text = res;
+                MREntry.Text = conf;
+                if (res.Contains("PR"))
+                {
+                    var sindex = res.LastIndexOf("PR") - 1;
+                    SexEntry.Text = res.Substring(sindex, 1);
+                }
+                if (res.Contains("PCP"))
+                {
+                    var nindex = res.LastIndexOf("PCP");
+                    NameEntry.Text = res.Substring(0, nindex - 1);
+                }
+                if (res.Contains("ACCT") & res.Contains("#")){
+                    var mindex2 = res.LastIndexOf("ACCT");
+                    var mindex = res.IndexOf("#");
+                    MREntry.Text = res.Substring(mindex + 1, mindex2 - mindex-1);
+                }
+                if(!(res.Contains("PR") | res.Contains("PCP") | res.Contains("ACCT") | res.Contains("#")))
+                {
+                    await DisplayAlert("Could not detect text.", "Try again or enter info manually.", "OK");
+                }
+            }
+            else
+            {
+
+                NameEntry.Text = "Image Recognition Failed";
+            }
+
+
+
+            
         }
+
+    
 
         private void SubmitButton_Clicked(object sender, EventArgs e)
         {
-
+            var name = NameEntry.Text;
+            var sex = SexEntry.Text;
+            var mr = MREntry.Text;
+            TestLabel.Text = name + " " + sex + " " + mr;
         }
 
         private void ContrastButton_Clicked(object sender, EventArgs e)
